74a69571e754a3c8ce364a30be0b4b333d394a71
codewithritik <97462674+codewithritik@users.noreply.github.com>
2022-02-27 19:50:10 +0530

    Flash and trending page is uploaded

---
+++ flase.js
@@ -0,0 +1,302 @@
+function fldatarr() {
+    let data = [{
+
+            image: `https://img2.hkrtcdn.com/14325/prd_1432481-ON-Optimum-Nutrition-Gold-Standard-100-Whey-Protein-2-lb-Double-Rich-Chocolate_o.jpg`,
+            title: `ON (Optimum Nutrition) Gold Standard 100% Whey Protein, 2 lb, Double Rich Chocolate`,
+            price: `3173`
+        }, {
+            image: `https://img1.hkrtcdn.com/18602/prd_1860160_o.jpg`,
+            title: `MB Fuel One Black Whey Protein, 1.65 lb, Chocolate`,
+            price: `1399`
+
+        }, {
+            image: `https://img2.hkrtcdn.com/13488/prd_1348781-MB-Fuel-One-Whey-Protein-Immunity-2.2-lb-Chocolate_o.jpg`,
+            title: `MB Fuel One Whey Protein Immunity+, 2.2 lb, Chocolate`,
+            price: `1799`
+        }, {
+            image: `https://img1.hkrtcdn.com/16743/prd_1674290-MuscleBlaze-High-Protein-Muesli-1-kg-Dark-Chocolate-Cranberry_o.jpg`,
+            title: `MuscleBlaze High Protein Muesli, 1 kg, Dark Chocolate & Cranberr`,
+            price: `649`
+
+        }, {
+            image: `https://img10.hkrtcdn.com/10673/prd_1067229-MuscleBlaze-BCAA-Pro-2-PiecesPack-30-Servings-Fruit-Splash-Watermelon_o.jpg`,
+            title: `MuscleBlaze BCAA Pro, 2 Piece(s)/Pack, Fruit Splash & Watermelon, 16 Servings`,
+            price: `899`
+        }, {
+            image: `https://img7.hkrtcdn.com/18427/prd_1842626-MuscleBlaze-Fish-Oil-Gold-with-Higher-Strength-EPA-DHA-60-capsules_o.jpg`,
+            title: `MuscleBlaze Fish Oil Gold with Higher Strength EPA & DHA, 60 capsules`,
+            price: `769`
+
+        }, {
+            image: `https://img6.hkrtcdn.com/14369/prd_1436875-MuscleBlaze-PRE-Workout-200-0.22-lb-Green-Apple_o.jpg`,
+            title: `MuscleBlaze PRE Workout 200, 0.22 lb, Green Apple`,
+            price: `699`
+        }, {
+            image: `https://img6.hkrtcdn.com/15090/prd_1508975-MuscleBlaze-Probiotic-Muesli-1-kg-FruitsNuts-Seeds_o.jpg`,
+            title: `MuscleBlaze Probiotic Muesli, 1 kg, Fruits,Nuts & Seeds`,
+            price: `599`
+
+        }, {
+            image: `https://img4.hkrtcdn.com/11674/prd_1167313_c_m.jpg`,
+            title: `MuscleBlaze Weight Gainer with Added Digezyme, 11 lb, Chocolate`,
+            price: `3799`
+
+        }, {
+            image: `https://img10.hkrtcdn.com/9815/prd_981429-MuscleBlaze-BCAA-Gold-811-0.99-lb-Raspberry-Lemonade_o.jpg`,
+            title: `MuscleBlaze BCAA Gold 8:1:1, 0.99 lb, Raspberry Lemonade, 30 Servings`,
+            price: `2099`
+        }, {
+            image: `https://img4.hkrtcdn.com/14624/prd_1462323-MuscleBlaze-Probiotic-Muesli-0.4-kg-FruitsNuts-Seeds_o.jpg`,
+            title: `MuscleBlaze Probiotic Muesli, 0.4 kg, Fruits,Nuts & Seeds`,
+            price: `260`
+
+        }, {
+            image: `https://img8.hkrtcdn.com/11827/prd_1182687-MuscleBlaze-Vite-Gold-25-sachetspack-Unflavoured_o.jpg`,
+            title: `MuscleBlaze MB Vite Gold, 125 tablet(s), Unflavoured`,
+            price: `1574`
+        }, {
+            image: `https://img6.hkrtcdn.com/6794/prd_679315-MuscleBlaze-Women-Daily-Multivitamin-60-tablets-Unflavoured_o.jpg`,
+            title: `MuscleBlaze Women Daily Multivitamin, 60 tablet(s), Unflavoured`,
+            price: `599`
+
+        }, {
+            image: `https://img4.hkrtcdn.com/15959/prd_1595883-MuscleBlaze-Sparkling-Protein-Water-10g-Protein-6-PiecesPack-Cola_o.jpg`,
+            title: `MuscleBlaze Sparkling Protein Water (10g Protein), 6 Piece(s)/Pack, Cola`,
+            price: `660`
+        }, {
+            image: `https://img2.hkrtcdn.com/7265/prd_726451-MuscleBlaze-IsoWhey-Women-Protein-0.88-lb-Chocolate_c_m.jpg`,
+            title: `MuscleBlaze Iso-Whey Women Protein, 0.88 lb, Chocolate`,
+            price: `1699`
+
+        }, {
+            image: `https://img10.hkrtcdn.com/14623/prd_1462289-MuscleBlaze-Probiotic-Muesli-0.4-kg-Berry_c_m.jpg`,
+            title: `MuscleBlaze Probiotic Muesli, 0.4 kg, Berry`,
+            price: `260`
+        }, {
+            image: `https://img4.hkrtcdn.com/5805/prd_580453_c_m.jpg`,
+            title: `MuscleBlaze Iso-Whey Women Protein, 2.2 lb, Chocolat`,
+            price: `3599`
+
+        }, {
+            image: `https://img8.hkrtcdn.com/12595/prd_1259487-MuscleBlaze-MiniProtein-Bar-10g-Protein-12-PiecesPack-Almond-Fudge_c_m.jpg`,
+            title: `MuscleBlaze Mini-Protein Bar (10g Protein), Choco Almond, 12 bar(s)`,
+            price: `590`
+        }, {
+            image: `https://img8.hkrtcdn.com/9082/prd_908117-MuscleBlaze-Whey-Gold-Protein-4.4-lb-Irish-Cream-Chocolate_c_m.jpg`,
+            title: `MuscleBlaze Whey Gold Protein, 4.4 lb, Irish Cream Chocolate`,
+            price: `6249`
+
+        }, {
+            image: `https://img4.hkrtcdn.com/13604/prd_1360353-MuscleBlaze-Beginners-Protein-2.2-lb-Magical-Mango_c_m.jpg`,
+            title: `MuscleBlaze BCAA Pro, 0.99 lb, Fruit Splash, 30 Servings`,
+            price: `1699`
+        }, {
+            image: `https://img8.hkrtcdn.com/15157/prd_1515607-Yours-Truly-Curcumin-Enriched-Immunity-Gummies-Mango-Flavour-30-gummies_c_m.jpg`,
+            title: `Yours Truly Curcumin Enriched Immunity Gummies Mango Flavour, 30 gummies`,
+            price: `339`
+
+        }, {
+            image: `https://img3.hkrtcdn.com/18457/prd_1845622-MuscleTech-Nitro-Tech-Whey-Protein-4.4-lb-Milk-Chocolate_c_m.jpg`,
+            title: `MuscleTech NitroTech Whey Protein, 4.4 lb, Milk Chocolate`,
+            price: `6299`
+        }, {
+            image: `https://img1.hkrtcdn.com/1691/prd_169080_c_m.jpg`,
+            title: `Ultimate Nutrition Prostar 100% Whey Protein, 5.28 lb, Chocolate Creme`,
+            price: `5749`
+        },
+        {
+            image: `https://img7.hkrtcdn.com/18582/prd_1858116_o.jpg`,
+            title: `MuscleBlaze Biozyme Performance Whey 4.4 lb, Choco Banana Smoothie`,
+            price: `1389`
+        }
+    ]
+
+    return data
+}
+
+
+
+
+function appendflase_data(datarr, main) {
+    datarr.forEach(function(element) {
+        let div = document.createElement("div")
+        let image = document.createElement("img")
+        image.src = element.image
+        let title = document.createElement("h4")
+        title.innerText = element.title
+
+
+        let x = genRand(4.5, 5, 1)
+        let rating = document.createElement("span")
+        rating.innerText = x
+        rating.className = "ratingspan"
+        let star = stars()
+
+        let stari = document.createElement("i")
+        stari.innerHTML = star
+
+        let y = genRand(5000, 4000, 0)
+        let review = document.createElement("span")
+        review.className = "reviewspan"
+        review.innerText = " " + "(" + y + ")"
+
+
+        let stardiv = document.createElement("div")
+        stardiv.append(stari, rating, review)
+        stardiv.id = "stardiv"
+        let pricediv = document.createElement("div")
+
+        let price = document.createElement("span")
+        price.innerHTML = "₹" + element.price
+        price.className = "price"
+
+
+        let quick = quickfuntion()
+        let quickbtn = document.createElement("div")
+        quickbtn.innerHTML = quick
+
+        pricediv.append(price, quickbtn)
+
+
+
+        quickbtn.className = "quickbtn"
+
+
+        pricediv.className = "pricediv"
+
+
+
+
+        div.append(image, title, stardiv, pricediv)
+        div.className = "dataofproduct"
+        div.onclick = () => {
+            localStorage.setItem("productdata", JSON.stringify(element))
+    
+            alert('Your item is added sucessfully')
+                // window.location.href = "./cart.html"
+        }
+
+
+        main.append(div)
+    })
+}
+
+function genRand(min, max, decimalPlaces) {
+    return (Math.random() * (max - min) + min).toFixed(decimalPlaces) * 1;
+}
+
+function stars() {
+    return ` 
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-half"></i>
+   `;
+}
+
+function quickfuntion() {
+    return `<button><i class="bi bi-lightning-charge-fill"></i>Quick Buy</button>`;
+
+}
+document.getElementById('sortByVariants').addEventListener('change', function() {
+    let data = fldatarr()
+    var option = this.value
+
+    if (option == "ASES") {
+        data.sort((a, b) => {
+            return Number(a.price) - Number(b.price);
+
+        })
+
+    } else if (option == "Popularity") {
+        window.location.herf = "./flase_sale.html"
+    } else {
+        //sortList(data,"DSE")
+        data.sort((a, b) => {
+            return Number(b.price) - Number(a.price);
+
+        })
+    }
+    sortdata(data)
+
+});
+
+// sort function
+function sortdata(datarr) {
+    let main = document.querySelector(".showappdata")
+    main.innerHTML = null
+    datarr.forEach(function(element) {
+        let div = document.createElement("div")
+        let image = document.createElement("img")
+        image.src = element.image
+        let title = document.createElement("h4")
+        title.innerText = element.title
+
+
+        let x = genRand(4.5, 5, 1)
+        let rating = document.createElement("span")
+        rating.innerText = x
+        rating.className = "ratingspan"
+        let star = stars()
+
+        let stari = document.createElement("i")
+        stari.innerHTML = star
+
+        let y = genRand(5000, 4000, 0)
+        let review = document.createElement("span")
+        review.className = "reviewspan"
+        review.innerText = " " + "(" + y + ")"
+
+
+        let stardiv = document.createElement("div")
+        stardiv.append(stari, rating, review)
+        stardiv.id = "stardiv"
+        let pricediv = document.createElement("div")
+
+        let price = document.createElement("span")
+        price.innerHTML = "₹" + element.price
+        price.className = "price"
+
+
+        let quick = quickfuntion()
+        let quickbtn = document.createElement("div")
+        quickbtn.innerHTML = quick
+
+        pricediv.append(price, quickbtn)
+
+
+
+        quickbtn.className = "quickbtn"
+
+
+        pricediv.className = "pricediv"
+
+
+
+
+        div.append(image, title, stardiv, pricediv)
+        div.className = "dataofproduct"
+        div.onclick = () => {
+            localStorage.setItem("productdata", JSON.stringify(element))
+            console.log("ritik")
+                //                window.location.href = "./productpage.html"
+        }
+
+
+        main.append(div)
+    })
+}
+
+
+
+
+
+
+
+// div.onclick = ()=>{
+//     localStorage.setItem("productdata",JSON.stringify(datarr))
+//     window.location.href = "./productpage.html"
+//     }
+
+export { fldatarr, appendflase_data }
---
+++ trendingdata.js
@@ -0,0 +1,314 @@
+function trendingdata(){
+
+let data_aditya = [
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhcHkG-WypTCUDapYf-j_yQF2PtJzR13GRXA&usqp=CAU",
+title:"TrueBasics Liver Detox with Sil;ybin Milk Thistle Extract",
+price:"1444" },
+
+{image:"https://img4.hkrtcdn.com/12049/prd_1204883-GNC-Milk-Thistle-250-mg-90-capsules_c_m.jpg",
+title:"GNC Milk Thistle 250mg",
+price:"974" },
+
+{image:"https://img7.hkrtcdn.com/2236/prd_223516_c_m.jpg",
+title:"Himalayan Organices Milk Thistle 800mg",
+price:"749" },
+
+{image:"https://img5.hkrtcdn.com/16741/prd_1674044_c_s.jpg",
+title:"TrueBasics sleep Easy with Melatonin Suntheanine",
+price:"565" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhcHkG-WypTCUDapYf-j_yQF2PtJzR13GRXA&usqp=CAU",
+title:"TrueBasics Eye Shield with Lute-gen Plus Beta Caro-gen",
+price:"589" },
+
+{image:"https://m.media-amazon.com/images/I/416gmbmAU6S.jpg",
+title:"HealthKart Milk Thistle",
+price:"499" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkvRtlgysEKign36hX01A0tO6NNg-8LFDHxvnQkC7RCjum_wBjvoyZbTk9IHJIH7CzAjg&usqp=CAU",
+title:"Muscle Blaze Milk Thistle",
+price:"329" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0TP8zhVnAg-LQmign-NoNQNJPyLgCLuoF6hLJMYMXuyfqFc2R9qcKe48RhtBCtWiNjk4&usqp=CAU",
+title:"HealthKart HK Vitals Melatonin",
+price:"199" },
+
+{image:"https://img4.hkrtcdn.com/10148/prd_1014783-Natures-Velvet-Milk-Thistle-Extract-60-softgels_o.jpg",
+title:"MuscleBlaze L-Arginine - 90Capsules",
+price:"699" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ2ZN8NKTSiCg7I-OVYlHoYKKeyKE-KaNvXLXkKP57waCyGct2y4pFqki7pN2U4FCJcLw&usqp=CAU",
+title:"Now L-Arginine(500mg)",
+price:"1298" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS10hVG7BvlA42nrFOSYXnVQKT4eNEb7ZrdZp10xVXQz6qYXEDB8rd5jpSuXlah8Md1Wvo&usqp=CAU",
+title:"Now Liver Refresh - 90capsules",
+price:"250" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxtYIxRsOUuDpPhy7RkW90HEN5MZpH41ZP92tOpGT1oYwT_5ItIEw1l-Nn0iSfumtVct8&usqp=CAU",
+title:"GNC L-Arginine(1000mg)",
+price:"180" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqmrl6Ke5Xo2mcLwF3gRIt0348xMCa8Of8n4GaI9aAISE2bWIfs8OPW45vORx-Zulz_Xs&usqp=CAU",
+title:"Big Muscles L-Arginine PMP Sex on The Beach",
+price:"379" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPaKX0wVyFqgSHQ8SB4CxOEL-PWW22WwHCG_oSPrebiXS5PsvvVeBsCTHeMJLa-8BtHUM&usqp=CAU",
+title:"INLIFE L-Arginine 1000mg - 60 veggie capsules(s)",
+price:"299" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRafnfxqoReAlPC3zj9iJglvwWB3ugbmzTwliaRVpUPJW2YXu30s0kOdZRmx6ai7jgVAJ4&usqp=CAU",
+title:"Health Oxide Liquid Formula",
+price:"450" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW34Srkaby_kcmG4B7k9de9i8_x8bqIXh3z2JtAHyeuo0ZRmirZBlRv56jOvYFReOLd1A&usqp=CAU",
+title:"Health XP Essential Series L Arginine L Citrulline",
+price:"399" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhcHkG-WypTCUDapYf-j_yQF2PtJzR13GRXA&usqp=CAU",
+title:"TrueBasics Liver Detox with Sil;ybin Milk Thistle Extract",
+price:"1444" },
+
+{image:"https://img4.hkrtcdn.com/12049/prd_1204883-GNC-Milk-Thistle-250-mg-90-capsules_c_m.jpg",
+title:"GNC Milk Thistle 250mg",
+price:"974" },
+
+{image:"https://img7.hkrtcdn.com/2236/prd_223516_c_m.jpg",
+title:"Himalayan Organices Milk Thistle 800mg",
+price:"749" },
+
+{image:"https://img5.hkrtcdn.com/16741/prd_1674044_c_s.jpg",
+title:"TrueBasics sleep Easy with Melatonin Suntheanine",
+price:"565" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhcHkG-WypTCUDapYf-j_yQF2PtJzR13GRXA&usqp=CAU",
+title:"TrueBasics Eye Shield with Lute-gen Plus Beta Caro-gen",
+price:"589" },
+
+{image:"https://m.media-amazon.com/images/I/416gmbmAU6S.jpg",
+title:"HealthKart Milk Thistle",
+price:"499" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkvRtlgysEKign36hX01A0tO6NNg-8LFDHxvnQkC7RCjum_wBjvoyZbTk9IHJIH7CzAjg&usqp=CAU",
+title:"Muscle Blaze Milk Thistle",
+price:"329" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0TP8zhVnAg-LQmign-NoNQNJPyLgCLuoF6hLJMYMXuyfqFc2R9qcKe48RhtBCtWiNjk4&usqp=CAU",
+title:"HealthKart HK Vitals Melatonin",
+price:"199" },
+
+{image:"https://img10.hkrtcdn.com/12294/pck_1229389_o.jpg",
+title:"MuscleBlaze L-Arginine - 90Capsules",
+price:"699" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ2ZN8NKTSiCg7I-OVYlHoYKKeyKE-KaNvXLXkKP57waCyGct2y4pFqki7pN2U4FCJcLw&usqp=CAU",
+title:"Now L-Arginine(500mg)",
+price:"1298" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS10hVG7BvlA42nrFOSYXnVQKT4eNEb7ZrdZp10xVXQz6qYXEDB8rd5jpSuXlah8Md1Wvo&usqp=CAU",
+title:"Now Liver Refresh - 90capsules",
+price:"250" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxtYIxRsOUuDpPhy7RkW90HEN5MZpH41ZP92tOpGT1oYwT_5ItIEw1l-Nn0iSfumtVct8&usqp=CAU",
+title:"GNC L-Arginine(1000mg)",
+price:"180" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqmrl6Ke5Xo2mcLwF3gRIt0348xMCa8Of8n4GaI9aAISE2bWIfs8OPW45vORx-Zulz_Xs&usqp=CAU",
+title:"Big Muscles L-Arginine PMP Sex on The Beach",
+price:"379" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPaKX0wVyFqgSHQ8SB4CxOEL-PWW22WwHCG_oSPrebiXS5PsvvVeBsCTHeMJLa-8BtHUM&usqp=CAU",
+title:"INLIFE L-Arginine 1000mg - 60 veggie capsules(s)",
+price:"299" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRafnfxqoReAlPC3zj9iJglvwWB3ugbmzTwliaRVpUPJW2YXu30s0kOdZRmx6ai7jgVAJ4&usqp=CAU",
+title:"Health Oxide Liquid Formula",
+price:"450" },
+
+{image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW34Srkaby_kcmG4B7k9de9i8_x8bqIXh3z2JtAHyeuo0ZRmirZBlRv56jOvYFReOLd1A&usqp=CAU",
+title:"Health XP Essential Series L Arginine L Citrulline",
+price:"399" }
+]
+ return data_aditya;
+}
+
+
+
+
+function appendflase_data(datarr,main){
+    datarr.map((element) => {
+            let div = document.createElement("div")
+            let image = document.createElement("img")
+            image.src = element.image
+            let title = document.createElement("h4")
+            title.innerText = element.title
+
+
+            let x = genRand(4.5, 5, 1)
+            let rating = document.createElement("span")
+            rating.innerText = x
+            rating.className = "ratingspan"
+            let star = stars()
+           
+            let stari = document.createElement("i")
+            stari.innerHTML = star
+
+            let y = genRand(5000, 4000, 0)
+            let review = document.createElement("span")
+            review.className = "reviewspan"
+            review.innerText = " "+"("+y+")"
+           
+          
+            let stardiv = document.createElement("div")
+            stardiv.append(stari,rating,review)
+            stardiv.id = "stardiv"
+            let pricediv = document.createElement("div")
+
+                let price = document.createElement("span")
+                price.innerHTML = "₹"+element.price
+                price.className ="price"
+
+            
+                let quick = quickfuntion()
+                let quickbtn = document.createElement("div")
+                quickbtn.innerHTML = quick
+
+                pricediv.append(price,quickbtn)
+                
+
+            
+            quickbtn.className = "quickbtn"
+
+            
+            pricediv.className = "pricediv"
+
+
+
+
+            div.append(image, title, stardiv ,pricediv)
+            div.className = "dataofproduct"
+            div.onclick = () => {
+                localStorage.setItem("productdata", JSON.stringify(element))
+                console.log("ritik")
+                alert('Your item is added sucessfully')
+                // window.location.href = "./cart.html"
+            }
+
+            main.append(div)
+        })
+}
+function genRand(min, max, decimalPlaces) {
+    return (Math.random() * (max - min) + min).toFixed(decimalPlaces) * 1;
+}
+
+function stars(){
+   return ` 
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-fill"></i>
+       <i class="bi bi-star-half"></i>
+   `;
+}
+
+function quickfuntion(){
+    return `<button><i class="bi bi-lightning-charge-fill"></i>Quick Buy</button>`;
+
+}
+
+
+document.getElementById('sortByVariants').addEventListener('change', function() {
+    let data = trendingdata()
+    var option = this.value
+
+    if(option =="ASES"){
+        data.sort((a,b)=>{
+            return Number(a.price) - Number(b.price);
+            
+        })
+       
+        }
+    else if(option == "Popularity"){
+        window.location.herf = "./trending.html"
+    }
+    else{
+        //sortList(data,"DSE")
+        data.sort((a,b)=>{
+            return Number(b.price) - Number(a.price);
+            
+        })
+    }
+    sortdata(data)
+    
+});
+
+// sort function
+function sortdata(datarr){
+    let main = document.querySelector(".showappdata")
+     main.innerHTML = null
+        datarr.forEach(function (element) {
+                let div = document.createElement("div")
+                let image = document.createElement("img")
+                image.src = element.image
+                let title = document.createElement("h4")
+                title.innerText = element.title
+    
+    
+                let x = genRand(4.5, 5, 1)
+                let rating = document.createElement("span")
+                rating.innerText = x
+                rating.className = "ratingspan"
+                let star = stars()
+    
+                let stari = document.createElement("i")
+                stari.innerHTML = star
+    
+                let y = genRand(5000, 4000, 0)
+                let review = document.createElement("span")
+                review.className = "reviewspan"
+                review.innerText = " " + "(" + y + ")"
+    
+    
+                let stardiv = document.createElement("div")
+                stardiv.append(stari, rating, review)
+                stardiv.id = "stardiv"
+                let pricediv = document.createElement("div")
+    
+                let price = document.createElement("span")
+                price.innerHTML = "₹" + element.price
+                price.className = "price"
+    
+    
+                let quick = quickfuntion()
+                let quickbtn = document.createElement("div")
+                quickbtn.innerHTML = quick
+    
+                pricediv.append(price, quickbtn)
+    
+    
+    
+                quickbtn.className = "quickbtn"
+    
+    
+                pricediv.className = "pricediv"
+    
+    
+    
+    
+                div.append(image, title, stardiv, pricediv)
+                div.className = "dataofproduct"
+                div.onclick = () => {
+                    localStorage.setItem("productdata", JSON.stringify(element))
+                    console.log("ritik")
+    //                window.location.href = "./productpage.html"
+                }
+    
+    
+                main.append(div)
+            })
+    }
+
+
+
+export {trendingdata,appendflase_data}
